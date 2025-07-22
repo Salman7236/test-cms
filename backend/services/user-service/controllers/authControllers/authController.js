0718d353656d2ffd61b1e507e10c30f4f215ed45
salman <muhammadsalman7236@gmail.com>
2025-07-22 10:45:39 +0500

    Get userLevel after logging in

--- backend/services/user-service/controllers/authControllers/authController.js
+++ backend/services/user-service/controllers/authControllers/authController.js
@@ -3,29 +3,33 @@ import { User } from "../../models/User.js";
 import { UserType } from "../../models/UserType.js";
 
 export const authController = async (req, res) => {
-    try {
-        const user = await User.findOne(
-            { userName: req.body.userName, userPwd: req.body.userPwd }
-        )
+  try {
+    const user = await User.findOne({
+      userName: req.body.userName,
+      userPwd: req.body.userPwd,
+    });
 
-        if (!user) return res.status(401).json({ error: 'Invalid credentials' })
+    if (!user) return res.status(401).json({ error: "Invalid credentials" });
 
-        const userTypeId = user.userTypeId;
+    const userTypeId = user.userTypeId;
 
-        if (!userTypeId) return res.status(400).json({ error: 'No user type entered' })
+    if (!userTypeId)
+      return res.status(400).json({ error: "No user type entered" });
 
-        const userType = await UserType.findById(
-            { _id: userTypeId }
-        )
+    const userType = await UserType.findById({ _id: userTypeId });
 
-        if (!userType) return res.status(404).json({ error: 'Invalid user type' })
+    if (!userType) return res.status(404).json({ error: "Invalid user type" });
 
-        return res.status(200).json({ 
-            message: "Login Successful",
-            token: 'jwt',
-            userTypeId
-        })
-    } catch (err) {
-        res.status(500).json({ error: 'Auth service error' })
-    }
-}
+    return res.status(200).json({
+      message: "Login Successful",
+      token: "jwt",
+      userTypeId,
+      userLevel: userType.userLevel,
+    });
+  } catch (err) {
+    console.error("Login error:", err);
+    return res
+      .status(500)
+      .json({ error: "Auth service error", details: err.message });
+  }
+};
